f1233726dfaefdf106037611cb909da80f7a05d0
adam <mada.carpm>
2024-11-13 16:39:08 +0200

    pi

--- CalcPi.java
+++ CalcPi.java
@@ -6,15 +6,15 @@ public class CalcPi {
 		double printedNum = 1;
 		double pi = Math.PI;
 		System.out.println(Math.PI);
-		while (userNum >0){
-			System.out.println(1/computNum +"  "+ 1/(computNum+2));
-
-			printedNum =printedNum - ((1/computNum) + (1/(computNum+2)));
-			userNum --;
-			System.out.println(computNum);
-			computNum +=4;
-		}
-		System.out.println(printedNum*4);
+		double sum=0;
+		double sign=1.0;
+		for(int i =1;i<=userNum;i+=2)
+			{
+				sum +=sign/i;
+				sign *=-1;
+			}
+		
+		System.out.println(sum*4);
 
 	}
 }
