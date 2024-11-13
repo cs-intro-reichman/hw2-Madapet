a1017379548c8bfe8a769167625657cca1219b31
adam <mada.carpm>
2024-11-13 14:50:12 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,18 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String userWord = args[0];
+                int num = Integer.parseInt(args[1]);
+                int i = userWord.length();
+                while (i>0){
+                        char letter = userWord.charAt(userWord.length()-i);
+                        System.out.println("Give me an "+letter+": "+letter+"!");
+                        i--;
+                }
+                System.out.println("What does at spell?");
+                while(num>0){
+                        System.out.println(userWord+"!!!");
+                        num--;
+                }
         }
 }
