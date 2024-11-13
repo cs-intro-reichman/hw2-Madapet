09395f867388f90b9df6ff2452b7405d33d212e9
adam <mada.carpm>
2024-11-13 17:09:11 +0200

    PI

--- CalcPi.java
+++ CalcPi.java
@@ -5,10 +5,12 @@ public class CalcPi {
 		System.out.println("Api according to Java: 3.141592653589793");
 		double sum=0;
 		double sign=1.0;
-		for(int i =1;i<=userNum;i+=2)
+		for(int i =1;userNum > 0;i+=2)
 			{
 				sum +=sign/i;
+				//System.out.println(i+ " " +sum*4);
 				sign *=-1;
+				userNum --;
 			}
 		
 		System.out.println("Api, approximated:	" +sum*4);
